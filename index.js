c1408a61535e87a5541dc91a92c15a814a6d618f
Joe Spedale <jspedale@gmail.com>
2017-07-23 11:57:40 -0400

    Using express static middleware to access css file.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/public', express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
